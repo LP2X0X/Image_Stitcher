8d30b1bd09e99db5960122a105ec3b7dee97227c
LP2X0X <phamquanglong301220@gmail.com>
2022-09-24 15:36:43 +0700

    Change histogram from second window to main window

--- Image_Stitcher/MainWindow.xaml.cs
+++ Image_Stitcher/MainWindow.xaml.cs
@@ -18,8 +18,9 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Runtime.Intrinsics.X86;
 using System.Collections.Generic;
-using System.Windows.Controls.DataVisualization.Charting;
 using System.ComponentModel;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 
 
@@ -29,17 +30,76 @@ namespace Image_Stitcher
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        // Public variables
+        // Create change property event and function
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string property_name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+        }
+
+        // Public variables for histogram
+        public int Min_H;
+        public int Max_H;
+        public int Max_A;
+        public int Min_A;
         public string data;
-        public int min_h;
-        public int max_h;
+
+        // Min histogram range
+        public int min_h
+        {
+            get { return Min_H; }
+            set
+            {
+                Min_H = value;
+                OnPropertyChanged("min_h");
+            }
+        }
+
+        // Max histogram range
+        public int max_h
+        {
+            get { return Max_H; }
+            set
+            {
+                Max_H = value;
+                OnPropertyChanged("max_h");
+            }
+        }
+
+        // Max histogram Y Axis
+        public int MaxAxisValue
+        {
+            get { return Max_A; }
+            set
+            {
+                Max_A = value;
+                OnPropertyChanged("MaxAxisValue");
+            }
+        }
+
+        // Min histogram Y Axis
+        public int MinAxisValue
+        {
+            get { return Min_A; }
+            set
+            {
+                Min_A = value;
+                OnPropertyChanged("MinAxisValue");
+            }
+        }
+
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
 
         public MainWindow()
         {
 
             InitializeComponent();
+            SeriesCollection = new SeriesCollection();
 
         }
         void Window_Closing(object sender, CancelEventArgs e)
@@ -252,15 +312,51 @@ namespace Image_Stitcher
 
         private void btn_histogram_Click(object sender, RoutedEventArgs e)
         {
-            // Get min, max data by user
-            min_h = Convert.ToInt32(select_min.Value);
-            max_h = Convert.ToInt32(select_max.Value);
 
             if (txt_path_histogram.Text != "")
             {
-                // Show histograms window
-                Histograms histograms = new Histograms();
-                histograms.Show();
+                // Clear previous chart
+                SeriesCollection.Clear();
+                SeriesCollection.Add
+            (
+                new ColumnSeries
+                {
+                    Title = "HISTOGRAM",
+                    Values = new ChartValues<int> { },
+                    ColumnPadding = 0,
+                }
+            );
+
+                Labels = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100",
+                "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146", "147", "148", "149", "150",
+                "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192", "193", "194", "195", "196", "197", "198", "199", "200",
+                "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212", "213", "214", "215", "216", "217", "218", "219", "220", "221", "222", "223", "224", "225", "226", "227", "228", "229", "230", "231", "232", "233", "234", "235", "236", "237", "238", "239", "240", "241", "242", "243", "244", "245", "246", "247", "248", "249", "250", "251", "252", "253", "254", "255"};
+
+                // Get histogram data from MainWindow
+                string[] values = data.Split(',');
+                int[] int_values = new int[values.Length];
+
+                // Convert value from string to int and add to chart
+                for ( int i = 0; i < values.Length; i++ )
+                {
+                    int y = Convert.ToInt32(values[i]);
+                    int_values[i] = y;
+                    SeriesCollection[0].Values.Add(y);
+                }
+
+                // Get min, max data by user
+                min_h = Convert.ToInt32(select_min.Value);
+                max_h = Convert.ToInt32(select_max.Value);
+
+
+                // Get min, max value for Y Axis
+                MaxAxisValue = int_values.Max();
+                MinAxisValue = 0;
+
+                Formatter = value => value.ToString("N");
+                DataContext = this;
+                
             }
             else
             {
@@ -270,8 +366,8 @@ namespace Image_Stitcher
                 Status_Box.AppendText("[" + d.ToString("dd/MM/yyyy") + " - " + d.ToString("HH:mm:ss") + "]: " + "[Error] Please select an image in order to calculate histogram.");
                 Status_Box.AppendText(Environment.NewLine);
             }
-            
-        }
+        
+    }
 
         private void btn_choose_Click(object sender, RoutedEventArgs e)
         {
